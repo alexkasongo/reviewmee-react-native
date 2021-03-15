b8eaac3927b9a2a2e2ae72fc19cd837fc0ec96cf
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-03-15 16:23:31 -0700

    added global styles sheet

--- screens/about.js
+++ screens/about.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function About() {
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Text>About</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-});
--- screens/home.js
+++ screens/home.js
@@ -1,20 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { globalStyles } from "../styles/global";
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>ReviewMee</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>ReviewMee</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-  titleText: {
-    fontFamily: "nunito-bold",
-    fontSize: 18,
-  },
-});
