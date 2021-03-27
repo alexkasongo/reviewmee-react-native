27b50cbac721a315c93d465226167b8a7407d256
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-03-26 18:13:22 -0700

    update

--- routes/drawer.js
+++ routes/drawer.js
@@ -21,3 +21,18 @@ const RootDrawerNavigator = createDrawerNavigator({
 });
 
 export default createAppContainer(RootDrawerNavigator);
+
+// const Drawer = createDrawerNavigator();
+
+// function Navigator() {
+//   return (
+// <Drawer.Navigator>
+//   <Drawer.Screen name="Login" component={Loginstack} />
+//   <Drawer.Screen name="Signup" component={Signupstack} />
+//   <Drawer.Screen name="Home" component={Homestack} />
+//   <Drawer.Screen name="About" component={AboutStack} />
+// </Drawer.Navigator>
+//   );
+// }
+
+// export default createAppContainer(Navigator);
