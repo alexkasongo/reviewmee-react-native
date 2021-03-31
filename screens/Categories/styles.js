95e45f496d14bbd66e0a2df40795e874c5ac80d5
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-03-30 17:37:20 -0700

    udpate

--- screens/Categories/styles.js
+++ screens/Categories/styles.js
@@ -1,43 +1,43 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   categoriesItemContainer: {
     flex: 1,
     margin: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     height: 215,
-    borderColor: '#cccccc',
+    borderColor: "#cccccc",
     borderWidth: 0.5,
     borderRadius: 20,
   },
   categoriesPhoto: {
-    width: '100%',
+    width: "100%",
     height: 155,
     borderRadius: 20,
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 0,
-    shadowColor: 'blue',
+    shadowColor: "blue",
     shadowOffset: {
       width: 0,
-      height: 3
+      height: 3,
     },
     shadowRadius: 5,
     shadowOpacity: 1.0,
-    elevation: 3
+    elevation: 3,
   },
   categoriesName: {
     flex: 1,
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    color: '#333333',
-    marginTop: 8
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "#333333",
+    marginTop: 8,
   },
   categoriesInfo: {
     marginTop: 3,
-    marginBottom: 5
-  }
+    marginBottom: 5,
+  },
 });
 
 export default styles;
