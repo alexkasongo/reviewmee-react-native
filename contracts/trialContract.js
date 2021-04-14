e66d4a7ed95783fb18fafc6d6425b32d16d2124d
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-04-13 23:19:57 -0700

    push

--- contracts/trialContract.js
+++ contracts/trialContract.js
@@ -1,11 +1,10 @@
 import React from "react";
+import ReactDOMServer from "react-dom/server";
 
 export default function trialContract() {
-  // return (
-  //   <h1 className="bg-secondary text-white text-center p-2">
-  //     Hello World and Aleko!
-  //   </h1>
-  // );
-
-  const html = `<h1>Hello Aleko and the world</h1>`;
+  return ReactDOMServer.renderToStaticMarkup(
+    <h1 className="bg-secondary text-white text-center p-2">
+      Hello World and Aleko!
+    </h1>
+  );
 }
