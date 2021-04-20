bfb19281a996d7fe69d72e3fb2f9043982eb6664
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-04-20 16:02:35 -0700

    update

--- screens/contracts/contractToSign.js
+++ screens/contracts/contractToSign.js
@@ -1,16 +1,69 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Signature from "react-native-signature-canvas";
 
-const ContractToSign = (props) => {
-  console.log(`contractToSign.js - 5 - variable`, props);
-  const contract = props.route.params.category;
+const ContractToSign = () => {
+  const [signature, setSign] = useState(null);
 
+  const handleSignature = (signature) => {
+    console.log(signature);
+    setSign(signature);
+  };
+
+  const handleEmpty = () => {
+    console.log("Empty");
+    setSign(null);
+  };
+
+  const style = `.m-signature-pad--footer
+    .button {
+      background-color: red;
+      color: #FFF;
+    }`;
   return (
-    <View>
-      <Text>Title: {contract.name}</Text>
-      <Text>Title: {contract.photo_url}</Text>
+    <View style={{ flex: 1 }}>
+      <View style={styles.preview}>
+        {signature ? (
+          <Image
+            resizeMode={"contain"}
+            style={{ width: "100%", height: "100%" }}
+            source={{ uri: signature }}
+          />
+        ) : null}
+      </View>
+      <Signature
+        onOK={handleSignature}
+        onEmpty={handleEmpty}
+        descriptionText="Sign"
+        clearText="Clear"
+        confirmText="Save"
+        webStyle={style}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  preview: {
+    width: "100%",
+    height: "50%",
+    backgroundColor: "#F8F8F8",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 15,
+  },
+  previewText: {
+    color: "#FFF",
+    fontSize: 14,
+    height: 40,
+    lineHeight: 40,
+    paddingLeft: 10,
+    paddingRight: 10,
+    backgroundColor: "#69B2FF",
+    width: 120,
+    textAlign: "center",
+    marginTop: 10,
+  },
+});
 
 export default ContractToSign;
