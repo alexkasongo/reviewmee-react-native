4bcf1fe056b2dc9bb0b9dc2191ec1e055470eeca
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-06-28 15:04:59 -0700

    up

--- screens/reviewForm.js
+++ screens/reviewForm.js
@@ -31,7 +31,7 @@ export default function ReviewForm({ addReview }) {
           // add the review here using the addReview() prop
           actions.resetForm();
           addReview(values);
-          // console.log(`reviewForm.js - 23 🔥`);
+          // console.log(`reviewForm.js - 23 🔥 up`);
         }}
       >
         {/* if validation fails, yup passes errors in props.errors below */}
